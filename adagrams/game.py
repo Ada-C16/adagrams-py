2c3281eee14d009dfa89d679738d0f2c48023fba
sarahstandish <z.sisyphusrocks@gmail.com>
2021-09-20 16:49:46 -0700

    add comments re which function is in which wave

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,13 +1,18 @@
+# Wave 1
 def draw_letters():
     pass
 
+# Wave 2
 def uses_available_letters(word, letter_bank):
     pass
 
+# Wave 3
 def score_word(word):
     pass
 
+# Wave 4
 def get_highest_word_score(word_list):
     pass
 
 # Mariah's comment added 9/20
+# Sarah's comment 9/20
