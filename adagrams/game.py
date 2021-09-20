8cd7e600c15c60eefc1dbaf4b65056b48ab8de8f
chrisss111 <christinecoding10@gmail.com>
2021-09-20 18:44:26 -0400

    Wave 1 completed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,24 @@
 def draw_letters():
-    pass
+
+    import random
+
+    main_letters_list = ["a", "a", "a", "a", "a", "a", "a", "a","a", "b", "b", "c", "c", "d", "d", "d", "d", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "f", "f", "g", "g", "g", "h", "h", "i", "i", "i", "i", "i", "i", "i", "i", "i", "j", "k", "l", "l", "l", "l", "m", "m", "n", "n", "n", "n", "n", "n", "o", "o", "o", "o", "o", "o", "o", "o", "p", "p", "q", "r", "r", "r", "r", "r", "r", "s", "s", "s", "s", "t", "t", "t", "t", "t", "t", "u", "u", "u", "u", "v", "v", "w", "w", "x", "y", "y", "z"]  
+
+    main_letters_list_copy = []
+    user_hand = []
+
+
+    for letter in main_letters_list:
+        main_letters_list_copy.append(letter)
+
+    x = 0
+    while x < 10:
+        letter = random.choice(main_letters_list_copy)
+        user_hand.append(letter.capitalize())
+        main_letters_list_copy.remove(letter)
+        x += 1
+
+    return user_hand
 
 def uses_available_letters(word, letter_bank):
     pass
