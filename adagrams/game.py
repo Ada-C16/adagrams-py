c2e9f5be9543f92d910a2fa32bfefbb8ec188239
Elly Wong <elly.yywong@gmail.com>
2021-09-20 11:57:22 -0700

    Finished Wave 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,9 @@
+import random
+
 def draw_letters():
-    pass
+    list_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    return random.choices(list_letters, weights = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1], k = 10)
+
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -9,3 +13,6 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+
+print(draw_letters())
