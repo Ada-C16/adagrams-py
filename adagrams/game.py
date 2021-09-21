1750352ebb2f5f403d15a38af9a5c911212890e9
Nicole Washington <nicolewashington.003@gmail.com>
2021-09-20 23:01:03 -0700

    Added draw_letters functionfor wave 01, with an implementation that passses wave 01 test

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,17 @@
+import random 
+
 def draw_letters():
-    pass
+    letter_pool = ['A','A','A','A','A','A','A','A','A',
+    'B','B', 'C','C', 'D','D','D','D',
+    'E','E','E','E','E','E','E','E','E','E','E','E', 
+    'F','F','G','G','G', 'H','H', 'I','I','I','I','I','I','I','I','I',
+    'J', 'K', 'L','L','L','L', 'M','M', 'N','N','N','N','N','N',
+    'O','O','O','O','O','O','O','O', 'P','P', 'Q', 'R','R','R','R','R','R',
+    'S','S','S','S', 'T','T','T','T','T','T', 'U','U','U','U',
+    'V','V', 'W','W', 'X', 'Y','Y','Z']
+    player_hand = random.sample(letter_pool, k=10)
+    return player_hand
+
 
 def uses_available_letters(word, letter_bank):
     pass
