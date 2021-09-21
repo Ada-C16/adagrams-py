a38481fdb5d216c9f5eb040908809320c67796ca
kaitlyngore <kaitlyngore@gmail.com>
2021-09-21 17:51:36 -0400

    passes wave 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,46 @@
+import random
+
+def dictionary_to_list(dict):
+    letter_list = []
+    for key, value in dict.items():
+        for i in range(value): 
+            letter_list.append(key)
+    
+    return letter_list
+
 def draw_letters():
-    pass
+    letter_pool = {
+        "A": 9,
+        "B": 2,
+        "C": 2,
+        "D": 4,
+        "E": 12,
+        "F": 2,
+        "G": 3,
+        "I": 9,
+        "J": 1,
+        "K": 1,
+        "L": 4,
+        "M": 2,
+        "N": 6,
+        "O": 8,
+        "P": 2,
+        "Q": 1,
+        "R": 6,
+        "S": 4,
+        "T": 6,
+        "U": 4,
+        "V": 2,
+        "W": 2,
+        "X": 1,
+        "Y": 2,
+        "Z": 1,
+    }
+
+    possible_letters = dictionary_to_list(letter_pool)
+
+    player_hand = random.sample(possible_letters, 10)
+    return player_hand
 
 def uses_available_letters(word, letter_bank):
     pass
