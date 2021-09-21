8005b1d1b380374039bcef1346d71b3b14fdbe65
dynamicdev3000 <michellemoralesjs@gmail.com>
2021-09-21 10:39:55 -0400

    wave 2 passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,11 +1,37 @@
-def draw_letters():
-    pass
+# from tests.test_wave_01 import LETTER_POOL
+
+
+# def draw_letters():
+
+def display_game_instructions():
+    print("Please input your submission for the longest anagram you can come up with")
+
+def display_needs_valid_input_message():
+    print("You entered in a word that contains characters not in the letter bank")
+    display_game_instructions()
 
 def uses_available_letters(word, letter_bank):
-    pass
+    display_game_instructions()
+    
+    user_letters = letter_bank.copy()
+    
+    try: 
+        for letter in word:
+            user_letters.remove(letter)
+        return True
+#if runs out of letter
+#remove() method raises an ValueError exception, if specified item doesn’t exist in a list.
+    except ValueError:
+        display_needs_valid_input_message()
+        return False 
 
-def score_word(word):
-    pass
-
-def get_highest_word_score(word_list):
-    pass
+# def score_word(word):
+#     """
+#     score = 0
+#     dictionary mapping - to refer table 
+#     add up value (sum)
+#     score += value
+#     return score int(sum)
+#     """
+# def get_highest_word_score(word_list):
+#     pass
