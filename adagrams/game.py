c2c8a674e51bebdc6d89f1c8279c48c7d905fcc9
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-09-21 10:41:31 -0700

    added function score_word

--- adagrams/game.py
+++ adagrams/game.py
@@ -50,7 +50,24 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    word = word.upper()
+    score = 0
+    SCORE_CHART = {
+    "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
+    "D": 2, "G": 2, 
+    "B": 3, "C": 3, "M": 3, "P": 3,
+    "F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+    "K": 5,
+    "J": 8, "X": 8,
+    "Q": 10, "Z": 10
+    }
+
+    for letter in word:
+        score += SCORE_CHART[letter]
+    if len(word) == 7 or len(word) == 8 or len(word) == 9 or len(word) == 10:
+        score = score + 8
+
+    return score
 
 def get_highest_word_score(word_list):
     pass
