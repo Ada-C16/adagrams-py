768e847db919bac2d942023bd423d8bf8fe6b50b
Makhabat <maksatbekova.m@gmail.com>
2021-09-21 10:33:42 -0700

    retuns list of random 10 letters

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,55 @@
+import random
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+def create_list_of_letters(LETTER_POOL):
+    list_of_letters = []
+    for letter, count in LETTER_POOL.items():
+        while count > 0:
+            list_of_letters.append(letter)
+            count = count - 1
+    return (list_of_letters)
+
+# create_list_of_letters(pool_of_letters)
+
 def draw_letters():
-    pass
+    list_of_letters = create_list_of_letters(LETTER_POOL)
+    list_of_random_letters = []
+    length_of_list = 10
+    while length_of_list > 0: 
+        random_index = random.randint(0, (len(list_of_letters)-1))
+        list_of_random_letters.append(list_of_letters[random_index])
+        list_of_letters.remove(list_of_letters[random_index])
+        length_of_list = length_of_list - 1
+    return (list_of_random_letters)
+
 
 def uses_available_letters(word, letter_bank):
     pass
