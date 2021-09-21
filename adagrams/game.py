009bc2ba091aa828bb8dd7bd01dfefed11746fc1
goblinbrooke <BrookeVlahos@gmail.com>
2021-09-21 11:31:02 -0700

    Wave 1 + Game Intro

--- adagrams/game.py
+++ adagrams/game.py
@@ -38,20 +38,11 @@ def draw_letters():
     return_list = []
     letter_freq = {}
     counter = 0
-    #we change it to a while loop to return ten random letters and their values.
+    
     while counter < 10:
-        #choose an integer position random between 1 and 26
-        #add LETTER_POOL[random int] to return_list
-            random_letter_index = random.randint(0,25)
-            #Then, grab just the keys of the dictionary
-            #And convert those to a list.
-            #Then grab the index of that list using the random_letter number (1-26)
-            #Because lists are 0-based, we need to subtract 1 otherwise we grab the letter before
-            #the one we expect to grab.
-            random_letter_val = list(LETTER_POOL.keys())[random_letter_index]
 
-            #Before appending the random letter, we need to look up and make sure
-            #it is not appearing more than the value in LETTER_POOL
+            random_letter_index = random.randint(0,25)
+            random_letter_val = list(LETTER_POOL.keys())[random_letter_index]
             if random_letter_val in letter_freq:
                 letter_freq[random_letter_val] += 1
             else:
@@ -61,25 +52,37 @@ def draw_letters():
                 return_list.append(random_letter_val)
                 counter += 1
 
-
-    # Create a for loop to return ten random letters and their values.
-    for i in range (0, 10):
-        # Choose an integer position random between 1 and 26
-        # Add LETTER_POOL[random int] to return_list
-        # For the letters: Make a list of all letters, then remove as they're generated. 
-        try:
-            random_letter = random.randint(1, 26)
-            random_letter_val = list(LETTER_POOL.keys())[random_letter]
-            return_list.append(random_letter_val)
-        except Exception as e:
-            print(f"exception:{str(e)}")
-
     return return_list
     
-    #
+print(draw_letters())
+
+    # Funtion 1 Comment Walkthrough:
+    # While loop to return 10 random letters and their values
+    # Choose an integer position random between 1 and 26
+    # Add LETTER_POOL[random int] to return_list
+    # Grab just the keys of the dictionary and convert those to a list.
+    # Then grab the index of that list using the random_letter number (1-26)
+    # Because lists are 0-based, we need to subtract 1 otherwise we grab the letter before
+    # Before appending the random letter, we need to look up and make sure it is not appearing more than the value in LETTER_POOL
+    # Adds selected letter to list
+    # Returns list
+
+    # First attempt in comments: 
+    # Create a for loop to return ten random letters and their values.
+    # for i in range (0, 10):
+    #     # Choose an integer position random between 1 and 26
+    #     # Add LETTER_POOL[random int] to return_list
+    #     # For the letters: Make a list of all letters, then remove as they're generated. 
+    #     try:
+    #         random_letter = random.randint(1, 26)
+    #         random_letter_val = list(LETTER_POOL.keys())[random_letter]
+    #         return_list.append(random_letter_val)
+    #     except Exception as e:
+    #         print(f"exception:{str(e)}")
 
 
 def uses_available_letters(word, letter_bank):
+    
     # Check to see if letters are in available word bank
     # Elif returns True if in bank, False if not
     # *REMOVES* from letter bank so does not CHANGE letter bank
