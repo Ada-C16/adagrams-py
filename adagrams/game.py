06f2cb36117dff6021e6df60a0e56578daf68b6c
Alma <abecerrilsalas@gmail.com>
2021-09-21 12:14:43 -0700

    made first draft to uses_available_letters function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,13 @@
+import random
+
 def draw_letters():
     pass
 
 def uses_available_letters(word, letter_bank):
-    pass
+    matched_list=[characters in letter_bank for characters in word]
+    string_contains_chars = all(matched_list)
+    return string_contains_chars
+
 
 def score_word(word):
     pass
