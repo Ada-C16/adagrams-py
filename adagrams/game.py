663254adcdf0c907222e803d585375a99708d844
Cecily <cecily.marroquin@gmail.com>
2021-09-21 15:38:44 -0400

    wave 1 pseudo

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,14 @@
 def draw_letters():
-    pass
+    # create a data structure for the distribution of letters
+    # while len of new data structure - list "letters" is < 10
+    #   note to self - double check length of 10 actually means 10
+    # use random() to pull a random letter
+    #   check if random letter is in our dataset 
+    #   check if quantity is > 0
+    #    if yes then return letter to list "letters" as single letter strings
+    #      qty value of letter decrease by 1
+    #    if not - continue 
+
 
 def uses_available_letters(word, letter_bank):
     pass
