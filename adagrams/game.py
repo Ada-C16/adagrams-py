85269198514a6cdabdb43ac863d2f96d6e127b19
Lilly <waterlilly169@GMAIL.COM>
2021-09-21 10:26:15 -0700

    changed wave 1 function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,37 +1,20 @@
 import random
 
 def draw_letters():
-    import random 
+    letter_bank = {'A':9, 'B':2, 'C':2, 'D': 4, 'E': 12, 'F': 2, 'G':3, 'H': 2, 'I': 9, 'J': 1, 'K':1, 'L':4, 'M': 2, 'N':6, 'O': 8, 'P':2, 'Q':1, 'R': 6, 'S': 4, 'T':6, 'U':4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z':1}
+    tile_bag = []
+    for k, v in letter_bank.items():
+        for each_letter in range(v):
+            tile_bag.append(k)
 
-letter_bank = {'A':9, 'B':2, 'C':2, 'D': 4, 'E': 12, 'F': 2, 'G':3, 'H': 2, 'I': 9, 'J': 1, 'K':1, 'L':4, 'M': 2, 'N':6, 'O': 8, 'P':2, 'Q':1, 'R': 6, 'S': 4, 'T':6, 'U':4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z':1}
+    tile_list = []
+    for ten_letters in range(10):
+        letter = random.choice(tile_bag)
+        tile_list.append(letter)
+        tile_bag.remove(letter)
 
-
-new_dict = {}
-for k, v in letter_bank.items():
-    new_dict[k] = v
-
-
-for x in range(10):
-    letter = random.choice(list(new_dict.keys()))
-    new_dict[letter] - 1
-    
-for key in letter_bank:
-    if key in letter:
-        letter_bank[key]
-
-print(new_dict)
-
-# def draw_letters():
-#     letter_bank = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'J', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'N', 'N', 'N', 'N', 'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P', 'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S', 'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z']
-
-#     array_letter = []
-#     for ten_letters in range(10):
-
-
-#         array_letter.append(random.choice(letter_bank))
-#     return letter_bank_modfied
-# print(draw_letters())
-    pass
+    return tile_list
+   
 
 def uses_available_letters(word, letter_bank):
     pass
