2ed08e03a88bb51c6670e00e441d6eddaa62546a
Jesse <jesseannepope@gmail.com>
2021-09-21 21:54:49 -0700

    wave one function started

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,47 @@
+import random
+
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    letters_left = LETTER_POOL #initializing local variable so it can be manipulated
+    choice_ten = [] #initializing empty list to hold randomly chosen letters
+    while len(choice_ten) < 10: #the final loop will result in 10 letters in the list
+        letter = random.choice(list(letters_left)) #randomly choosing letter from dictionary
+        if letters_left[letter] > 0: # making sure there are still letters in the bag
+            choice_ten.append(letter) #adding chosen letter to list
+            letters_left[letter] -= 1 #short hand to say variable is now equal to variable minus one 
+                                      # ex: letters_left[letter] = letters_left[letter] - 1
+    # print(choice_ten)
+    # print(letters_left)
+        
+        
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -9,3 +51,5 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+draw_letters()
