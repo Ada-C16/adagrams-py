41461b53e1f90c7f591ce4fa8b6f324563b08344
Laurel S <ls60@protonmail.com>
2021-09-22 12:40:40 -0700

    Syntax clean up.

--- adagrams/game.py
+++ adagrams/game.py
@@ -59,77 +59,57 @@ LETTER_SCORE = {
 }
 
 def draw_letters():
-    # initialize list of letters
+    # create list of randomly selected letters from letter pool
     chosen_letters = []
-    remaining_letters_count = 0
-    for letter_count in LETTER_POOL.values():
-        remaining_letters_count += letter_count
+    remaining_letters_count = sum(LETTER_POOL.values())
     if remaining_letters_count >= 10:
-    # do this 10 times:
+        # do this 10 times:
         while len(chosen_letters) != 10:
             # from letter bank select random letter
-            drawn_letter = key, value = random.choice(list(LETTER_POOL.items()))
-            # check value of letter is not zero
+            drawn_letter = random.choice(list(LETTER_POOL.items()))
+            # check count of letter is not zero
             if LETTER_POOL[drawn_letter[0]] != 0:
                 # add letter to list
                 chosen_letters.append(drawn_letter[0])
-                # if chosen, decrease value by 1
+                # decrease letter pool count by 1
                 LETTER_POOL[drawn_letter[0]] -= 1
     return chosen_letters
 
 def uses_available_letters(word, letter_bank):
     # checks to make sure that word is composed of elements in letter_bank
-    word = word.upper()
     letter_bank_copy = letter_bank[:]
-    if isinstance(word, str) and len(word) <= 10:
-        for char in word:
-            if char in letter_bank_copy:
-                letter_bank_copy.remove(char)
-            else:
-                return False
-        return True
-    return False
-    # make a copy of letter_bank    
-    # ensure word is a string of 10 or less char
-    # loop through the word char by char
-    # if in letter_bank copy, proceed - if not in letter bank copy, return false
-    # (loop through original list, but as we loop, we check the copy)
-    # remove char from letter_bank copy
-    # return True
+    if not isinstance(word, str) and len(word) <= 10:
+        return False
+    # if word is valid    
+    for char in word.upper():
+        if char in letter_bank_copy:
+            letter_bank_copy.remove(char)
+        else:
+            return False
+    return True
 
 def score_word(word):
-    #inputs user input (word) returns score
-    total_word_score=0
-    word = word.upper()
-    for char in word:
+    #inputs user word and returns score
+    total_word_score = 0
+    for char in word.upper():
         total_word_score += LETTER_SCORE[char]
-    if len(word) >=7:
+    if len(word) >= 7:
         total_word_score += 8
     return total_word_score
 
-    #initialize total word score = 0
-    #iterate through word
-    #for each letter, access in dictionary
-    #add value to total word score
-    #if word length is >=7, add 8 points
-
 def get_highest_word_score(word_list):
-    # dictionary of word scores
-    word_scores_dict = {}
-    for word in word_list:
-        word_scores_dict[word] = score_word(word)
-    highest_score = max(word_scores_dict.values())
-    
-    highest_score_words = [word for word in word_list if word_scores_dict[word] == highest_score]
-    if len(highest_score_words) > 1:
-        return tie_breaker(highest_score_words, word_scores_dict)
-    winning_word = highest_score_words[0]
-    return winning_word, word_scores_dict[winning_word]
+    # returns highest scoring word and its score
+    word_scores_dict = {word: score_word(word) for word in word_list}
+    high_score = max(word_scores_dict.values())
+    high_score_words = [word for word in word_list if word_scores_dict[word] == high_score]
+    if len(high_score_words) > 1:
+        return tie_breaker(high_score_words, high_score)
+    winning_word = high_score_words[0]
+    return winning_word, high_score
 
-def tie_breaker(highest_score_words, word_scores_dict):
-    for word in highest_score_words:
+def tie_breaker(word_list, score):
+    for word in word_list:
         if len(word) == 10:
-            return word, word_scores_dict[word]
-        # otherwise, just choose shorter word
-    winning_word = min((word for word in highest_score_words), key= len)
-    return winning_word, word_scores_dict[winning_word]
+            return word, score
+    winning_word = min((word for word in word_list), key= len)
+    return winning_word, score
