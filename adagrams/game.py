88954d91f7d505c085f0215da819d7aa9efe47f3
karishma <karishmacarterjohnson@gmail.com>
2021-09-22 14:34:52 -0700

    revise doc string wave1; use dictionary to build letter pool

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,32 +1,48 @@
 def draw_letters():
     '''
-    Your first task is to build a hand of 10 letters for the user. To do so, implement the function `draw_letters` in `game.py`. This method should have the following properties:
+    build a hand of 10 letters for the user
 
-    - No parameters
-    - Returns an array of ten strings
-    - Each string should contain exactly one letter
+    - Returns an array of ten strings (one letter each)
     - These represent the hand of letters that the player has drawn
     - The letters should be randomly drawn from a pool of letters
-    - This letter pool should reflect the distribution of letters as described in the table below
-    - There are only 2 available `C` letters, so `draw_letters` cannot ever return more than 2 `C`s
-    - Since there are 12 `E`s but only 1 `Z`, it should be 12 times as likely for the user to draw an `E` as a `Z`
-    - Invoking this function should **not** change the pool of letters
-    - Imagine that the user returns their hand to the pool before drawing new letters
     '''
     import random 
-
+    # drawling pool
+    available_tiles = {
+        'A': 9, 
+        'B': 2, 
+        'C': 2, 
+        'D': 4, 
+        'E': 12, 
+        'F': 2, 
+        'G': 3, 
+        'H': 2, 
+        'I': 9, 
+        'J': 1, 
+        'K': 1, 
+        'L': 4, 
+        'M': 2, 
+        'N': 6, 
+        'O': 18, 
+        'P': 2, 
+        'Q': 1, 
+        'R': 6, 
+        'S': 4, 
+        'T': 6, 
+        'U': 4, 
+        'V': 2, 
+        'W': 2, 
+        'X': 1, 
+        'Y': 2, 
+        'Z': 1
+        }
     pool = []
-    pool += "A"*9 + "B"*2 + "C"*2 + \
-            "D"*4 + "E"*12 + "F"*2 + \
-            "G"*3 + "H"*2 + "I"*9 + \
-            "J" + "K" + "L"*4+ "M"*2 + \
-            "N"*6 + "O"*8 + "P"*2 + \
-            "Q" + "R"*6 + "S"*4 + "T"*6 + \
-            "U"*4 + "V"*2 + "W"*2 + "X" + "Y"*2 + "Z"
     
-    n = 10
+    for letter, count in available_tiles.items():
+        pool += letter *count
     
-    return random.sample(pool, n)
+    # randomly select from pool
+    return random.sample(pool, 10)
 
 
 import copy
@@ -136,6 +152,11 @@ def get_highest_word_score(word_list):
         # if there is a 10
         if tie_chart[first_max] == 10:
             winning_word = [k for k,v in tie_chart.items() if v == 10][0]
+            # winning_word = []
+            # for k, v in tie_chart.items():
+            #   if v == 10:
+            #       winning_word.append(k)
+            #
         else: # no tens, default to lowest score
             winning_word = [k for k,v in tie_chart.items() if v == tie_chart[first_min]][0]
         winner = [winning_word, score_board[winning_word]]
