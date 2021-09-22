2d750ed9059253ed3eda2189c357a9de779a48a6
Shaina Beth <shainabeth94@gmail.com>
2021-09-22 14:20:22 -0700

    completed draw_letters function by randomly choosing a letter and adding it to the returned draw_ten list

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,57 @@
+import random
+
 def draw_letters():
-    pass
+    pool_of_letters = {
+        'A': 9,
+        'B': 2,
+        'C': 2,
+        'D': 4,
+        'E': 12,
+        'F': 2,
+        'G': 3,
+        'H': 2,
+        'I': 9,
+        'J': 1,
+        'K': 1,
+        'L': 4,
+        'M': 2,
+        'N': 6,
+        'O': 8,
+        'P': 2,
+        'Q': 1,
+        'R': 6,
+        'S': 4,
+        'T': 6,
+        'U': 4,
+        'V': 2,
+        'W': 2,
+        'X': 1,
+        'Y': 2,
+        'Z': 1
+    }
+
+    list_of_letters = list(pool_of_letters.keys())
+
+    draw_ten = []
+
+
+
+
+    for i in range(10):
+        random_letter = random.randint(0, 25)
+        letter = list_of_letters[random_letter]
+        
+        while pool_of_letters[letter] == 0:
+            random_letter = random.randint(0, 25)
+            letter = list_of_letters[random_letter]
+        
+        draw_ten.append(letter)
+        pool_of_letters[letter] -= 1
+
+        
+    return draw_ten
+
+
 
 def uses_available_letters(word, letter_bank):
     pass
