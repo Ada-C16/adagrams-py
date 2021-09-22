7b039e1754eb51a6f93f07b5c6108ae4a4778f09
brew doctor <>
2021-09-22 15:32:02 +0200

    completed wave 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,33 @@
 def draw_letters():
-    pass
+
+  letters_list=[]
+  import random
+  letters_dict = {'A' : 9, 'N' : 6, 'B' : 2, 	'O' : 8,
+'C' : 2, 	'P' : 2,
+'D' : 4, 	'Q' : 1,
+'E' : 12, 	'R' : 6,
+'F' : 2, 	'S' : 4,
+'G' : 3, 	'T' : 6,
+'H' : 2, 	'U' : 4,
+'I' : 9, 	'V' : 2,
+'J' : 1, 	'W' : 2,
+'K' : 1, 	'X' : 1,
+'L' : 4, 	'Y' : 2,
+'M' : 2, 	'Z' : 1}
+  l=len(letters_list)
+  for l in range (0,10):
+    letter=random.choice(list(letters_dict.keys()))
+    letter=letter.upper()
+    letters_list.append(letter)
+    for key,value in letters_dict.items():
+      if letters_list.count(key) >= value:
+        letters_list.remove(key)
+        letter=random.choice(list(letters_dict.keys()))
+        letter=letter.upper()
+        letters_list.append(letter)
+  return letters_list
+  
+
 
 def uses_available_letters(word, letter_bank):
     pass
