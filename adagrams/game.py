b6a004c167ca7dcf42193757d6b342275a30614f
Sabrina Lauredan <slauredan@gmail.com>
2021-09-22 15:12:39 -0400

    "passing wave three"

--- adagrams/game.py
+++ adagrams/game.py
@@ -64,10 +64,38 @@ def draw_letters():
 def uses_available_letters(word, letter_bank):
     pass
 
+SCORES = {"A": 1, "C": 3, "B": 3, "E": 1, "D": 2, "G": 2, 
+    "F": 4, "I": 1, "H": 4, "K": 5, "J": 8, "M": 3, 
+    "L": 1, "O": 1, "N": 1, "Q": 10, "P": 3, "S": 1, 
+    "R": 1, "U": 1, "T": 1, "W": 4, "V": 4, "Y": 4, 
+    "X": 8, "Z": 10}
 
-####stl to pseucode 
 def score_word(word):
-    pass
+    '''
+    returns the score of a given word as defined by the adagrams game
+    1. input parameter: word (str of chars)
+    2. returns an int representing the no. of points
+
+    #Each letter within `word` has a point value. 
+    #The number of points of each letter is summed up to represent the total score of `word`
+    ~~~~~~If the length of the word is 7, 8, 9, or 10, then the word gets an additional 8 points
+    '''
+    # return sum(SCORES[letter] for letter in word) 
+    # would like to refactor to the above 
+
+    total = 0 
+    # for char in word: 
+    if len(word) >= 7:
+            total +=8   
+    # for letter in word.upper():       
+    #     total += SCORES[letter]  
+    total += sum(SCORES[letter] for letter in word.upper())      
+    return total      
+
+
 
 def get_highest_word_score(word_list):
+    '''
+    
+    '''
     pass
