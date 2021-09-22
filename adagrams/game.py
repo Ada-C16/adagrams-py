e0636f1c790ee9da4da1e01f5f8263c4147317e8
Veronica Osei <veronicaosei273@gmail.com>
2021-09-21 23:20:12 -0500

    2 test ass function 2

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,34 @@
 def draw_letters():
     pass
+#LETTER_POOL = {"letter" : #of letter}
+
+#letter_bank = []
+
+#loop through letter_pool to get 10 random letters and append them to the letter_bank list
+#for letter in lETTER_POOL:
+    #if len(letter_bank) < 10:
+#random_letter = random.choice(#Letter_bank is dict)
+#consider using random.sample method
+#letter_bank.append(random_letter)
+
+#def function_2:
+#letter_freq = {}
+
+#for letter in letters:
+    #if letter in letter_freq:
+        #letter_freq[letter] += 1
+    #else:
+        #letter_freq[letter] = 1
+
 
 def uses_available_letters(word, letter_bank):
     pass
+    letter = ""
+    
+    if letter in word and word == letter_bank:
+        return True
+    else:
+        return False
 
 def score_word(word):
     pass
