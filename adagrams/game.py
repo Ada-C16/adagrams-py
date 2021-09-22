d50c722f78bdedcabfc39c3101242f9d314513b0
karishma <karishmacarterjohnson@gmail.com>
2021-09-22 14:39:38 -0700

    shorten get_highest_word_score doc string

--- adagrams/game.py
+++ adagrams/game.py
@@ -115,19 +115,11 @@ def score_word(word):
 def get_highest_word_score(word_list):
     ''' 
     `get_highest_score` in `game.py` has the following properties:
-
     - Has one parameter: `word_list`, which is a list of strings
-    - Returns a tuple that represents the data of a winning word and it's score.  
-      The tuple must contain the following elements:
-        - index 0 ([0]): a string of a word
-        - index 1 ([1]): the score of that word
-    - In the case of tie in scores, use these tie-breaking rules:
-        - prefer the word with the fewest letters...
-        - ...unless one word has 10 letters. If the top score is tied between 
-        multiple words and one is 10 letters long, choose the one with 10 
-        letters over the one with fewer tiles
-        - If the there are multiple words that are the same score and the 
-        same length, pick the first one in the supplied list
+    - Returns the data of a winning word and it's score.  
+    - Tie Breaker:
+        - if 10 letters, it wins (choose first one if there are multiple)
+        - if no ten letter, choose fewest char word (first one if there are multiple)
     '''
     # build score board for all words submitted
     score_board = {word:score_word(word) for word in word_list}
