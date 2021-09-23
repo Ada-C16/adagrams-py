ace0c1aa2ec41be01e0d46f15770e4973607350f
Angela <angelfan123@gmail.com>
2021-09-23 08:30:13 -0700

    score_words complete

--- adagrams/game.py
+++ adagrams/game.py
@@ -40,6 +40,7 @@ def draw_letters():
     while len(letters_drawn) != 10:
         random_entry = random.choice(letter_pool_list)
         letters_drawn.append(random_entry)
+        letter_pool_list.remove(random_entry)
     return letters_drawn
 
 
@@ -62,8 +63,63 @@ def draw_letters():
 def uses_available_letters(word, letter_bank):
     pass
 
-def score_word(word):
+# return a score (int) of inputted word
+    # create score chart data structure
+        # dictionary with score int as the key and a list of string letters as the values
+    #for each letter in the word
+    #   find the letter in the score chart
+        # will need to access the list of letters
+        # for each score value (key)
+        #   access the letter which is the value and a list
+        #   iteratre thru the list to flag if the letter exists
+        #  perhaps use continue?? when letter is found
+    #   add up the points from each letter in word
+    #   define var for total points (count)
+    # count length of word
+    #   if length 7 -10 letters long, add 8 pts
     pass
+
+def score_word(word):
+    
+    score_chart = {
+    
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
+
+    word = word.upper()
+    total_points = 0
+
+    for letter in word:
+        total_points += score_chart[letter]
+    if len(word) >= 7 and len(word) <= 10:
+        total_points += 8
+    
+    return total_points
 
 def get_highest_word_score(word_list):
     pass
