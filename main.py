efa06f34b2aa2c73f80bcd38f8aaa37c94d4d4aa
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-09-23 11:08:05 -0700

    prettier print of best word(s)

--- main.py
+++ main.py
@@ -108,7 +108,8 @@ def wave_5_run_game():
         display_score(score)
         all_possible = finds_all_possible_words_with_scores(letter_bank)
         best_score, best_words = gets_best_possible(all_possible)
-        print(f"The best possible word(s): {best_words} with a score of {best_score}.")
+        separator = ", "
+        print(f"The best possible word(s): {separator.join(best_words)} with a score of {best_score}.")
         played_words.append(user_input_word)
 
         display_retry_instructions()
