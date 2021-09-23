698e4ce7659865ad0d81fc72552e6433c516da62
Veronica Osei <veronicaosei273@gmail.com>
2021-09-23 16:11:04 -0500

    updated Wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,9 @@
 
 import random
 def draw_letters():
-    LETTER_POOL = {}
+    letter_pool = {}
+    #deep-copy of letter_pool to prevent it from changing as we withdraw letters from it
+
 
     letter_bank = []
     letter_list = list(LETTER_POOL.keys())
@@ -40,46 +42,49 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    
-    score_chart ={
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
-    word_dict = {}
-    for letter in word:
-        word_dict.append[letter]
 
     score_of_points = 0
 
+    score_chart = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
+
+    word = word.upper()
+    
+
     for letter in word:
-        score_of_points =+ score_chart[letter]
-    if len(word) >= 7 and len(word) <= 10:
+        score_of_points += score_chart[letter]
+    if len(word) in range(7,11):
         score_of_points += 8
     return score_of_points
+
+
+
 
 def get_highest_word_score(word_list):
     pass
