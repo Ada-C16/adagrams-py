f2a896acc49a47e59e5f75e8bc836f349585f4d3
Lilly <waterlilly169@GMAIL.COM>
2021-09-22 18:33:41 -0700

    final draft

--- adagrams/game.py
+++ adagrams/game.py
@@ -2,12 +2,16 @@ import random
 # from tests.test_wave_01 import LETTER_POOL
 
 def draw_letters():
+    #creating a dictionary of letter pool
     letter_bank = {'A':9, 'B':2, 'C':2, 'D': 4, 'E': 12, 'F': 2, 'G':3, 'H': 2, 'I': 9, 'J': 1, 'K':1, 'L':4, 'M': 2, 'N':6, 'O': 8, 'P':2, 'Q':1, 'R': 6, 'S': 4, 'T':6, 'U':4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z':1}
+    
+    #get all of all possible letters
     tile_bag = []
-    for k, v in letter_bank.items():
-        for each_letter in range(v):
-            tile_bag.append(k)
+    for letter, freq in letter_bank.items():
+        for each_letter in range(freq):
+            tile_bag.append(letter)
 
+    #creates ten tiles from string from the list of letters
     tile_list = []
     for ten_letters in range(10):
         letter = random.choice(tile_bag)
@@ -18,49 +22,18 @@ def draw_letters():
    
 
 def uses_available_letters(word, letter_bank):
+    #verifies if word is in letterbank and if there are enough tiles
     letter_list = [letter for letter in letter_bank if letter in word]
     if len(letter_list) == len(word):
         return True
     return False
-
-    # let_list =[]
-    # for letter in letter_bank:
-    #     if letter in word:
-    #         n = letter_bank[letter_bank.index(letter)]
-    #         let_list.append(n)
-    #         if len(let_list) == len(word):
-    #             return True
-    # return False
-
-
-
-    # freq_dict={}
-    # counter_word={}
-    # for one_letter in word:
-    #     if one_letter not in letter_bank:
-    #         return False
-    #     else:
-    #         if one_letter in counter_word:
-    #             counter_word[one_letter] += 1
-    #         else:
-    #             counter_word[one_letter] = 1
-    # for letter in letter_bank:
-    #     if letter in freq_dict:
-    #         freq_dict[letter]+=1
-    #     else:
-    #         freq_dict[letter]=1
-    # if freq_dict[one_letter]>= counter_word[one_letter]:
-    #    one_letter=True
-    # else:
-    #     one_letter=False
-    # return one_letter
-
-
-    
+  
 
 def score_word(word):
+    #creates empty variable for total/makes input all upper case
     total=0
     modified_word=word.upper()
+    #Creates tile scores
     value1=["A","E","I","O","U","L","N", "R", "S", "T"]
     value_2=["D", "G"]
     value_3=["B", "C", "M", "P"]
@@ -69,6 +42,7 @@ def score_word(word):
     value_8=["J", "X"]
     value_10=["Q", "Z"]
 
+    #Assigns score for each letter in word, and gets total updating it 
     for letter in modified_word:
         if letter in value1:
             total+=1
@@ -89,33 +63,33 @@ def score_word(word):
     return(total)
 
 def get_highest_word_score(word_list):
+    #Get word dictionary with words and point value
     word_dict = {}
     for word in word_list:
         word_dict[word] = score_word(word) 
 
-   
+   #gets the max value in dictionary
     max_value = max(word_dict.values())
+    #get duplicates if present
     max_values = [k for k, v in word_dict.items() if v == max_value]
-
+    #gets min word length
     min_word_length = min(len(k) for k, v in word_dict.items())
+    #gets name of the max key 
     max_key = max(word_dict, key=word_dict.get)
 
-
     if len(max_values) > 1:
-
-        for k ,v in word_dict.items():
-          tie_breaker = []
-          if len(k) == 10:
-            tie_breaker.append(k)
-            tie_breaker.append(v)
-            return tuple(tie_breaker)
-          elif len(k) == min_word_length and len(k) != 10:
-              tie_breaker.append(k)
-              tie_breaker.append(v)
-
-        return tuple(tie_breaker)
+        #loopping over dictionary to find tie breaking winner
+        tie_breaker = []
+        for word , points in word_dict.items():
+          #if ever see word with 10 instant winner 
+          if len(word) == 10:
+            tie_breaker = (word, points)
+            return tie_breaker
+          #if if there is no word with 10 it will print the word with the fewest letters
+          elif len(word) == min_word_length:
+              tie_breaker = (word, points)   
+        return tie_breaker
+    # if no duplicates print single winner 
     else:
       return max_key, word_dict[max_key]    
   
-    
-    # return tuple(tie_breaker)    
