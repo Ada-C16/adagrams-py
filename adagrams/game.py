a7cf95797712e51b527a1f8e419a40659001f0ed
Rhyannon <rhyannonjoy@gmail.com>
2021-09-24 10:26:33 -0600

    Passes All Waves

--- adagrams/game.py
+++ adagrams/game.py
@@ -75,6 +75,10 @@ def score_word(word):
 
 # Wave 4
 def get_score_dict(word_list):
+    """
+    Takes one parameter word_list,
+    returns dictionary score_dict
+    """
     score_dict = {}
     for word in word_list:
         word_score = score_word(word)
@@ -85,31 +89,18 @@ def get_score_dict(word_list):
 def get_highest_word_score(word_list):
     """
     Takes one parameter word_list to find the highest scoring word,
-    returns the winning word tuple: (winning_word, top_score)
+    returns winning word tuple: (winning_word, top_score)
     """
     score_dictionary = get_score_dict(word_list)
     top_word = max(score_dictionary, key=score_dictionary.get)
     all_scores = get_score_dict(word_list).values()
     top_word_score = max(all_scores)
     
-    ties_list = [(top_word, top_word_score)]
-    for word, score in score_dictionary.items():
-        if top_word_score == score:
+    for word, score in score_dictionary.items(): 
+        if top_word_score == score and len(top_word) != 10:
             if len(word) < len(top_word):
-                ties_list.insert(0,(word, score))
-            elif len(word) == len(top_word):
-                ties_list.append((word, score))
+                top_word = word
             elif len(word) == 10:
-               ties_list.clear() 
-               ties_list.append((word, score))
-            
-    highest_word_score = ties_list[0]
-    return highest_word_score
-    
-    # tie-breaking rules:
-        # prefer word with fewest letters
-        # if top_score is btwn multiple words
-            # word with len() == 10 is winning_word
-        # if multiple words have same total_score, same len()
-            # store these in a tie_list
-            # winning_word == tie_list[0]
+                top_word = word
+
+    return top_word, top_word_score
