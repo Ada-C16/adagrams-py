125a2593fef0ef6f484245087790595ce24e6db9
Reid Maute <reidhowdy@gmail.com>
2021-09-24 06:33:37 -0700

    through wave 2

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,8 +1,73 @@
 def draw_letters():
-    pass
+    import random
+    
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+    }
+    
+    letter_list = []
+    for letter in LETTER_POOL:
+        counter = 0
+        while counter < LETTER_POOL[letter]:
+            letter_list.append(letter)
+            counter += 1
+            
+    
+    random.shuffle(letter_list)
+    
+    return letter_list[0:10]
+
 
 def uses_available_letters(word, letter_bank):
-    pass
+    letter_bank_copy = []
+    for item in letter_bank:
+        letter_bank_copy.append(item)
+
+    for char in word:
+        if char in letter_bank_copy:
+            letter_bank_copy.remove(char)
+        else:
+            return False
+
+    return True
+
+"""
+I have a string word
+I want to loop through the chars in word
+check to see if theyre in letter bank
+if they are, remove from letter bank
+
+keep checking until I return false
+
+if you go through all of the letters and check them, return true
+
+
+"""
 
 def score_word(word):
     pass
