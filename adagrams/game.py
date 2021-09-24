5acfccdc224df9ee0b36970c3b46080756ae5273
Veronica Osei <veronicaosei273@gmail.com>
2021-09-24 13:38:13 -0500

    Final Adagrams Project

--- adagrams/game.py
+++ adagrams/game.py
@@ -31,16 +31,39 @@ LETTER_POOL = {
 
 
 def draw_letters():
+    """The purpose of draw letters function is to draw 10
+    random letters from the letter pool. 
+    Returns:
+        After looping through the items within the letter pool
+        we drew a random sample of 10 letters and appended it to the
+        letter list to return our randomly drawn letters.
+    """
 
     letter_list = []
 
     for letter, frequency in LETTER_POOL.items():
-        for _ in range(frequency):
-            letter_list.append(letter)
-    return random.sample(letter_list, 10)
+            for i in range(frequency):
+                letter_list.append(letter)
+    return random.sample(letter_list, 10) # chose to use Random.sample for random sampling without replacement. 
+    
 
 
 def uses_available_letters(word, letter_bank):
+    """ The purpose of the uses_available_letters function is
+    to check to make sure there are enough letters in the letter 
+    bank to be drawn for a word.
+
+    Args:
+        word (string): input word
+        letter_bank (list): list of drawn letters in a hand. 
+        A list that is comprise of 10 strings each representing a 
+        letter.
+
+    Returns:
+        Boolean: Returns True if every letter in the input word is 
+        available in the right quantities in letter bank. It returns false if it 
+        does not meet those conditions.
+    """    
     letter_frequency = {}
 
     for letter in letter_bank:
@@ -60,7 +83,15 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
+    """ The score_word function calculates the points of a given word.
+    Each word has a point system and add up for each word. 
 
+    Args:
+        word (string): string of characters in each word.
+
+    Returns:
+        [integer]: calculates the amount in score_of_points variable.
+    """
     score_of_points = 0
 
     score_chart = {
@@ -102,6 +133,17 @@ def score_word(word):
 
 
 def get_highest_word_score(word_list):
+    """ The get_highest_word_score function calculates 
+    the word in the list that has the highest score. There
+    are exceptions for the word to be a winning word.
+
+    Args:
+        word_list (list): list of strings 
+
+    Returns:
+        [string]: The string of the winning word is returned.
+        [integer]: The score_of_points attached to the word. 
+    """    
     winning_word = ""
     highest_score = 0
 
