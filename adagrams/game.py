8b1289b1bfebbdac13c43599adc1f7fb85bf9f38
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-26 13:56:53 -0500

    added docstrings for get_highest_word_score function. refactored this function as well

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,23 +1,22 @@
 import random
-LETTER_POOL = {
-    'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1,
-    'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4,
-    'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
-}
 
 
 def draw_letters():
     """
-    Build a hand of 10 letters for the user. Returns an array of strings
-    with one letter in each string. The num of strings included in the array 
-    cannot be more than the num alloted to that letter in the letter table
+    Build a hand of 10 letters for the user. Returns an array of strings, one letter long.
+    The num of strings included in the array 
+    cannot be more than the num alloted in the letter table
     """
+    LETTER_POOL = {
+        'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1,
+        'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4,
+        'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1
+    }
     letter_to_draw = list(LETTER_POOL.keys())
     letters_drawn = []
     letter_count = {}
     for letter in random.sample(letter_to_draw, 10):
         if letter not in letters_drawn:
-            letter_count[letter] = 0
             letters_drawn.append(letter)
             letter_count[letter] = 1
         elif letter_count[letter] < LETTER_POOL[letter]:
@@ -29,15 +28,12 @@ def draw_letters():
 
 def uses_available_letters(word, letter_bank):
     """
-    This function will compare the letters in the user's word to the letter_bank.
-    If letters in user's word are not in letter_bank, or if the user uses more letters
-    than they have, the function will return False. 
-    Function will return False if the user doesn't empty a word. Else, it returns True.
+    If letters in user's word are not in letter_bank, or if user uses more letters
+    than in hand, the function returns False. 
+    Function returns False if user enters empty word. Else, it returns True.
     """
     for letter in word:
-        if letter not in letter_bank:
-            is_valid = False
-        if word.count(letter) > letter_bank.count(letter):
+        if letter not in letter_bank or word.count(letter) > letter_bank.count(letter):
             is_valid = False
         else:
             is_valid = True
@@ -47,9 +43,8 @@ def uses_available_letters(word, letter_bank):
 
 def score_word(word):
     """
-    This function will calculate the user's score for their entered word.
-    It needs to calculate a score that ignores the lettercase, takes into
-    account an empty word, and gives extra points for longer words.
+    Calculate  user's score for their entered word.
+    Ignore lettercase, account for empty word, and give extra points for longer words.
     """
     LETTER_POINTS = {
         'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8,
@@ -65,24 +60,30 @@ def score_word(word):
 
 
 def get_highest_word_score(word_list):
+    """
+    Score the words in word_list. Returns tuple of word with highest score.
+    If tie score, word with 10 letters or word with least letters wins.
+    """
     words_scored_dict = {user_word: score_word(
         user_word) for user_word in word_list}
-    winning_list = []
-    winning_combo = None
+
+    highest_word_score = max(words_scored_dict.values())
+    smallest_word_len = min((len(key) for key in words_scored_dict.keys()))
+    largest_word_len = max((len(key) for key in words_scored_dict.keys()))
+
     for user_word, score in words_scored_dict.items():
-        if len(user_word) == 10:
-            return user_word, score  # maybe highest_word_score == score instead
-        else:
-            if score == max(words_scored_dict.values()):
-                if len(user_word) < 10:
-                    winning_list.append((user_word, score))
-    max_word_len = 0
-    for word_combo in winning_list:
-        if len(word_combo[0]) > max_word_len:
-            max_word_len = len(word_combo[0])
-        if len(word_combo[0]) < max_word_len:
-            winning_combo = word_combo
-            return winning_combo
-        else:
-            winning_combo = winning_list[0]
-    return winning_combo
+        if largest_word_len == 10:
+            if len(user_word) != 10:
+                continue
+            elif len(user_word) == 10:
+                winning_word = user_word
+                highest_word_score = score
+                break
+        elif score == highest_word_score and len(
+                user_word) == smallest_word_len:
+            winning_word = user_word
+            break
+        elif score == highest_word_score:
+            winning_word = user_word
+
+    return winning_word, highest_word_score
