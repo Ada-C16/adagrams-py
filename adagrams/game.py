8b8300fa155705c3a49a403c7def8d976604883b
starseed2021 <tiff.harper18@gmail.com>
2021-11-24 09:55:58 -0600

    Passed All Waves

--- adagrams/game.py
+++ adagrams/game.py
@@ -4,7 +4,6 @@ LETTER_POOL = {'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2,
     'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 
     'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 
     'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1}
-
 SCORE_CHART = {
     ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T"): 1,
     ("D", "G") : 2,
@@ -14,6 +13,7 @@ SCORE_CHART = {
     ("J", "X") : 8,
     ("Q", "Z") : 10,
 }
+
 
 # Wave 1
 def draw_letters():
@@ -34,6 +34,7 @@ def draw_letters():
         i += 1
     return letters
 
+
 # Wave 2
 def uses_available_letters(word, letter_bank):
     """
@@ -43,14 +44,13 @@ def uses_available_letters(word, letter_bank):
     """
     #this method copies the letter bank list
     letter_bank_duplicate = letter_bank[:]
-
     for letter in word:
         if letter in letter_bank_duplicate:
             letter_bank_duplicate.remove(letter)
         else:
             return False
-
     return True
+
 
 # Wave 3        
 def score_word(word):
@@ -75,6 +75,10 @@ def score_word(word):
 
 # Wave 4
 def get_score_dict(word_list):
+    """
+    Takes one parameter word_list,
+    returns dictionary score_dict
+    """
     score_dict = {}
     for word in word_list:
         word_score = score_word(word)
@@ -85,35 +89,18 @@ def get_score_dict(word_list):
 def get_highest_word_score(word_list):
     """
     Takes one parameter word_list to find the highest scoring word,
-    returns the winning word in a tuple: (winning_word, top_score)
+    returns winning word tuple: (winning_word, top_score)
     """
     score_dictionary = get_score_dict(word_list)
     top_word = max(score_dictionary, key=score_dictionary.get)
     all_scores = get_score_dict(word_list).values()
     top_word_score = max(all_scores)
-    
-    ties_list = [(top_word, top_word_score)]
-    for word, score in score_dictionary.items():
-        if top_word_score == score:
+
+
+    for word, score in score_dictionary.items(): 
+        if top_word_score == score and len(top_word) != 10:
             if len(word) < len(top_word):
-                ties_list.insert(0,(word, score))
-            elif len(word) == len(top_word):
-                ties_list.append((word, score))
+                top_word = word
             elif len(word) == 10:
-               ties_list.insert(0, (word, score))
-            #else:
-            #    ties_list.insert(0, (top_word, top_word_score))
-        
-    highest_score = ties_list[0]
-    return highest_score
-
-
-
-    # word_list: list of strings
-    # tie-breaking rules:
-        # prefer word with fewest letters
-        # if top_score is btwn multiple words
-            # word with len() == 10 is winning_word
-        # if multiple words have same total_score, same len()
-            # store these in a tie_list
-            # winning_word == tie_list[0]
+                top_word = word
+    return top_word, top_word_score
